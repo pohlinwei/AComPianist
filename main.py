7a58e78195e9cb31cc33d16d7f71e724bf92e77b
pohlinwei <linwei0909@hotmail.com>
2019-08-05 15:34:54 +0800

    Included prediction and average hue

--- main.py
+++ main.py
@@ -1,15 +1,21 @@
 # check for errors and exceptions associated with import
 try:
-    import sys, os, time
+    import sys, os
     import urllib.request
+    from keras.models import load_model
     from preprocess_image import preprocess
 except ImportError as import_err:
     print('Import error: ' + str(import_err))
 except Exception as err:
     print('Exception detected: ' + str(err))
 
+# trained model
+MODEL_PATH = 'predict_emo.h5'
 # store user uploaded image here temporarily 
 TEMP_FILE_PATH = 'uploaded_image.jpeg'
+# input dimension for neural net
+DIM = 1010
+
 def data_url_to_image(data_url):
     data = urllib.request.urlopen(data_url)
     with open(TEMP_FILE_PATH, 'wb') as f:
@@ -18,23 +24,12 @@ def data_url_to_image(data_url):
 
 if __name__ == "__main__": 
     img_data_url = sys.argv[1]
-    start = time.time()
     try:
         data_url_to_image(img_data_url)
-        x = preprocess(TEMP_FILE_PATH)
-        # !!! to-do: add in prediction
+        x, avg_hue = preprocess(TEMP_FILE_PATH)
+        model = load_model(MODEL_PATH)
+        y = model.predict(x.reshape(1, DIM))
+        positive = (y[0] < 0.5)[0]
+        print([positive,avg_hue])
     except Exception as ex:
         print(ex) 
-    end = time.time()
-    print('time taken: ' + str(end - start))
-
-
-
-
-
-
-
-
-
-
-
